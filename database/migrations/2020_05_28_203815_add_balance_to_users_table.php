a81003219bceab3ae7e63dfd36f1cae74338e45e
p0tpan <bpare003@gmail.com>
2020-05-28 16:45:21 -0400

    edit home page to show 'balance' and update add_balance_to_users_table to function

--- database/migrations/2020_05_28_203815_add_balance_to_users_table.php
+++ database/migrations/2020_05_28_203815_add_balance_to_users_table.php
@@ -14,7 +14,7 @@ class AddBalanceToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->int('Balance');
         });
     }
 
@@ -26,7 +26,7 @@ class AddBalanceToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('Balance');
         });
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Account Balance: 
                 </div>
             </div>
         </div>
